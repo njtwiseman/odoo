357f0acb15271a4887e3b6e48059a6bad095e0a7
Nick.desktop <njtwiseman@gmail.com>
2019-05-14 12:10:17 -0700

    added invoice model

---
+++ addons/oreilly_parts/models/invoice.py
@@ -0,0 +1,51 @@
+
+# -*- coding =  utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class OreillyInvoice(models.Model):
+     accountNumber  
+     accountType 
+     authorizationMessage  
+     cashTendered  
+     changeReturned  
+     chargeAccount  
+     chargeCard  
+     checkedBy  
+     checkNumber  
+     counterNumber  
+     creditMemo  
+     customerOrderNumber  
+     customerType  
+     filledBy  
+     formattedInvoiceNumber  
+     invoiceJUDate  
+     invoiceDate  
+     invoiceDiscount  
+     invoiceNumber  
+     invoiceTotal  
+     invoiceType  
+     lastPage  
+     lineItems  
+     multiplePages  
+     originalInvoiceNumber  
+     originalPurchaseJUDate  
+     originalPurchaseDate  
+     originalSalesman  
+     pageNumber  
+     paymentOnAccount  
+     purchaserName  
+     shippingAddress  
+     shipVia  
+     specialInstructions  
+     storeAddress  
+     storeNumber  
+     storePhoneNumber  
+     timeOfOrder  
+     totalTax  
+     transType  
+     type  
+     valid  
+     whsNumber  
+
