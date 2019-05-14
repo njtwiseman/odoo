95e5946577c559ebad285822c1dcc53e67f5c2c5
Nick.desktop <njtwiseman@gmail.com>
2019-05-14 07:05:08 -0700

    Beginning adding an 'Oreilly' module

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -37,28 +37,28 @@ class ProductTemplate(models.Model):
     sequence = fields.Integer('Sequence', default=1, help='Gives the sequence order when displaying a product list')
     description = fields.Text(
         'Description', translate=True)
-    description_purchase = fields.Text(
-        'Purchase Description', translate=True)
-    description_sale = fields.Text(
-        'Sale Description', translate=True,
-        help="A description of the Product that you want to communicate to your customers. "
-             "This description will be copied to every Sales Order, Delivery Order and Customer Invoice/Credit Note")
-    type = fields.Selection([
-        ('consu', 'Consumable'),
-        ('service', 'Service')], string='Product Type', default='consu', required=True,
-        help='A storable product is a product for which you manage stock. The Inventory app has to be installed.\n'
-             'A consumable product is a product for which stock is not managed.\n'
-             'A service is a non-material product you provide.')
-    rental = fields.Boolean('Can be Rent')
-    categ_id = fields.Many2one(
-        'product.category', 'Product Category',
-        change_default=True, default=_get_default_category_id,
-        required=True, help="Select category for the current product")
-
-    currency_id = fields.Many2one(
-        'res.currency', 'Currency', compute='_compute_currency_id')
-    cost_currency_id = fields.Many2one(
-        'res.currency', 'Cost Currency', compute='_compute_cost_currency_id')
+#    description_purchase = fields.Text(
+#        'Purchase Description', translate=True)
+#    description_sale = fields.Text(
+#        'Sale Description', translate=True,
+#        help="A description of the Product that you want to communicate to your customers. "
+#             "This description will be copied to every Sales Order, Delivery Order and Customer Invoice/Credit Note")
+#    type = fields.Selection([
+#        ('consu', 'Consumable'),
+#        ('service', 'Service')], string='Product Type', default='consu', required=True,
+#        help='A storable product is a product for which you manage stock. The Inventory app has to be installed.\n'
+#             'A consumable product is a product for which stock is not managed.\n'
+#             'A service is a non-material product you provide.')
+#    rental = fields.Boolean('Can be Rent')
+#    categ_id = fields.Many2one(
+#        'product.category', 'Product Category',
+#        change_default=True, default=_get_default_category_id,
+#        required=True, help="Select category for the current product")
+#
+#    currency_id = fields.Many2one(
+#        'res.currency', 'Currency', compute='_compute_currency_id')
+#    cost_currency_id = fields.Many2one(
+#        'res.currency', 'Cost Currency', compute='_compute_cost_currency_id')
 
     # price fields
     # price: total template price, context dependent (partner, pricelist, quantity)
@@ -92,9 +92,9 @@ class ProductTemplate(models.Model):
 
     sale_ok = fields.Boolean('Can be Sold', default=True)
     purchase_ok = fields.Boolean('Can be Purchased', default=True)
-    pricelist_id = fields.Many2one(
-        'product.pricelist', 'Pricelist', store=False,
-        help='Technical field. Used for searching on pricelists, not stored in database.')
+#    pricelist_id = fields.Many2one(
+#        'product.pricelist', 'Pricelist', store=False,
+#        help='Technical field. Used for searching on pricelists, not stored in database.')
     uom_id = fields.Many2one(
         'uom.uom', 'Unit of Measure',
         default=_get_default_uom_id, required=True,
@@ -137,19 +137,19 @@ class ProductTemplate(models.Model):
     valid_existing_variant_ids = fields.Many2many('product.product',
         compute="_compute_valid_existing_variant_ids", string='Valid Existing Variants', help="Technical compute")
 
-    product_variant_ids = fields.One2many('product.product', 'product_tmpl_id', 'Products', required=True)
-    # performance: product_variant_id provides prefetching on the first product variant only
-    product_variant_id = fields.Many2one('product.product', 'Product', compute='_compute_product_variant_id')
-
-    product_variant_count = fields.Integer(
-        '# Product Variants', compute='_compute_product_variant_count')
-
-    # related to display product product information if is_product_variant
-    barcode = fields.Char('Barcode', oldname='ean13', related='product_variant_ids.barcode', readonly=False)
-    default_code = fields.Char(
-        'Internal Reference', compute='_compute_default_code',
-        inverse='_set_default_code', store=True)
-
+#    product_variant_ids = fields.One2many('product.product', 'product_tmpl_id', 'Products', required=True)
+#    # performance: product_variant_id provides prefetching on the first product variant only
+#    product_variant_id = fields.Many2one('product.product', 'Product', compute='_compute_product_variant_id')
+#
+#    product_variant_count = fields.Integer(
+#        '# Product Variants', compute='_compute_product_variant_count')
+#
+#    # related to display product product information if is_product_variant
+#    barcode = fields.Char('Barcode', oldname='ean13', related='product_variant_ids.barcode', readonly=False)
+#    default_code = fields.Char(
+#        'Internal Reference', compute='_compute_default_code',
+#        inverse='_set_default_code', store=True)
+#
     item_ids = fields.One2many('product.pricelist.item', 'product_tmpl_id', 'Pricelist Items')
 
     # image: all image fields are base64 encoded and PIL-supported
