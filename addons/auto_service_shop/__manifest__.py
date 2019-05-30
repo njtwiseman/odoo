962c517de06b02b0f2710d9286f5b61f91ee8a76
Nick.desktop <njtwiseman@gmail.com>
2019-05-30 00:42:29 -0700

    added auto_service_shop module

---
+++ addons/auto_service_shop/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2018-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#    you can modify it under the terms of the GNU AGPL (v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AGPL (AGPL v3) for more details.
+#
+##############################################################################
+{
+    'name': 'Auto Service Management',
+    'version': '12.0.1.0.0',
+    'summary': 'Module for managing auto service shop daily activities.',
+    'category': 'Industries',
+    'author': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['stock_account', 'mail', 'product', 'account'],
+    'data': ['security/security.xml',
+             'security/ir.model.access.csv',
+             'views/auto_service_views.xml',
+             'wizard/auto_create_invoice_views.xml',
+             'reports/auto_service_ticket.xml',
+             'reports/service_ticket_template.xml',
+             'data/auto_service_data.xml',
+             'data/auto_service_email_template.xml'],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
