0eeed51c3fc7fd9260b69523d6c241aca8f69098
Nick Work <njtwiseman@gmail.com>
2019-05-30 15:46:54 -0700

    removed old modules

--- addons/oreilly_parts/models/part.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class OreillyPart(models.Model):
-    _name = 'oreilly_parts.part'
-
-    
--- addons/oreilly_parts/models/vehicle.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class OreillyVehicle(models.Model):
-    _name = 'oreilly.vehicle' 
-#    name = fields.Char('Name', index=True, required=True, translate=True)
